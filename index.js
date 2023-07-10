8f80307b80a3f9c8076d32eadae53db8175bf64c
Nehema <+>
2023-07-10 18:42:13 +0300

    my project

---
+++ index.js
@@ -0,0 +1,39 @@
+//Fetch the JSON data
+fetch('db.json')
+  .then(response => response.json())
+  .then(data => {
+      displayCharacters(data.animals);
+  })
+  .catch(error => {
+      console.log(error);
+  });
+
+//function to displayCharacters
+function displayCharacters(characters) {
+  let animals = document.getElementById("jungle");
+
+  characters.forEach(character => {
+      let newDiv = document.createElement("div");
+      newDiv.classList = "jungle";
+      newDiv.innerHTML = character.name;
+      animals.appendChild(newDiv);
+      newDiv.addEventListener("click", function () {
+          displayCharacterDetails(character);
+      });
+  });
+}
+
+//function to dislayCharacterDetails
+function displayCharacterDetails(character) {
+  let animalDetails = document.getElementById("animal_details");
+  animalDetails.innerHTML = "";
+
+  const image = document.createElement("img");
+  image.src = character.image;
+  image.alt = character.name;
+  animalDetails.appendChild(image);
+
+  const votes = document.createElement("p");
+  votes.textContent = "Votes: " + character.votes;
+  animalDetails.appendChild(votes);
+}
